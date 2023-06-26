c11991246073a9a34080b21de71707671bc38505
Rahul Sahu <classelite27@Rahuls-MacBook-Air.local>
2023-06-26 20:52:23 +0530

    add cryptography and caesar cipher

---
+++ index.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+    $("#Cryptography").trigger("click");
+});
+
+
+var state = {
+    "Cryptography": {
+        "total_page": 1,
+        "active_page": 1
+    },
+    "CaesarCipher": {
+        "total_page": 2,
+        "active_page": 1
+    },
+    "DiffieHellman": {
+        "total_page": 2,
+        "active_page": 1
+    }
+};
+
+
+$("nav span").on("click", function() {
+    // Make all nav links 'inactive' when anyone of them is clicked.
+    $("nav span.active").removeClass("active");
+
+    var id = $(this).attr("id");
+
+    // Make this nav link 'active'.
+    $(`#${id}`).addClass("active");
+    $(".textBox").load(`./algorithms/${id}${state[id]["active_page"]}.html`);
+    
+    var blob = "";
+    for(var i = 1; i <= state[id]["total_page"]; i++) {
+        
+        var active = "";
+        if(state[id]["active_page"] === i){
+            active = " active";
+        }
+
+        blob += `<li class="page-item${active}"><a class="page-link" href="#">${i}</a></li>`;
+    }
+
+    $(".pagination").html(blob);
+});
+
+
+$(".pagination").on("click", ".page-item", function() {
+    $(".pagination .page-item.active").removeClass("active");
+
+    var elem = $(this);
+    elem.addClass("active");
+
+    var id = $("nav span.active").attr("id");
+    state[id]["active_page"] = parseInt(elem.text());
+    $(".textBox").load(`./algorithms/${id}${state[id]["active_page"]}.html`);
+});
